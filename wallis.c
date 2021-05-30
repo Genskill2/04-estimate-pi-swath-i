685961f789ff09f057786654b8f4dea3c73b6867
Swathi <72941440+swath-i@users.noreply.github.com>
2021-05-30 20:40:25 +0530

    Wallis.c

--- wallis.c
+++ wallis.c
@@ -3,17 +3,18 @@
 #include <stdio.h>
 #include <math.h>
 
-float wallis_pi(int);
+float wallis_pi(int n) 
+{
+float ans=1.0;
+for(int i=1;i<=n;i++) 
+{float nr= 4*i*i;
 
-int main(void) {
-  float pi;
-  for (int i=0; i<5; i++) {
-    pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) > 0.15)) {
-      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
-      abort();
-    }
-  }
+ans= ans*(nr/(nr-1));
+
+} 
+
+return ans*2; 
+}
 
   for (int i=500; i<3000; i++) {
     pi = wallis_pi(i);
